d9b42a73fb5b1e785d3da7de4c7d2ad358194641
Vinh <vinhbuith32412000@gmail.com>
2026-03-23 10:00:58 +0700

    add integration test for server internal flow

---
+++ in_memory_player_store.go
@@ -0,0 +1,22 @@
+package main
+
+import "sync"
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{scores: map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	scores map[string]int
+	mu     sync.Mutex
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.scores[name]
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.scores[name]++
+}
